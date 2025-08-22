44a56b48650be5d921920a318d7a3c3a9f2305c2
Meghakt123 <ktmegha10@gmail.com>
2025-08-22 15:13:30 +0530

    fix: manual shift hours

--- craft_hr/events/shift_type.py
+++ craft_hr/events/shift_type.py
@@ -7,15 +7,23 @@ def before_validate(doc, method):
     manually_add = frappe.db.get_single_value("Craft HR Settings", "manually_add_shift_hours")
 
     if manually_add:
-        if not doc.manual_shift_hours:
-            frappe.throw("Please enter Manual Shift Hours since the setting is enabled.")
-        shift_hours = doc.manual_shift_hours
+        if doc.manual_shift_hours:
+            shift_hours = doc.manual_shift_hours
+        else:
+            start = doc.start_time
+            end = doc.end_time
+            if isinstance(start, str):
+                start = datetime.datetime.strptime(start, "%H:%M:%S")
+                end = datetime.datetime.strptime(end, "%H:%M:%S")
+            if doc.is_night_shift:
+                end += timedelta(hours=24)
+            shift_hours = time_diff_in_hours(end, start)
     else:
         start = doc.start_time
         end = doc.end_time
-        if isinstance(doc.start_time, str):
-            start = datetime.datetime.strptime(doc.start_time, "%H:%M:%S")
-            end = datetime.datetime.strptime(doc.end_time, "%H:%M:%S")
+        if isinstance(start, str):
+            start = datetime.datetime.strptime(start, "%H:%M:%S")
+            end = datetime.datetime.strptime(end, "%H:%M:%S")
         if doc.is_night_shift:
             end += timedelta(hours=24)
         shift_hours = time_diff_in_hours(end, start)
