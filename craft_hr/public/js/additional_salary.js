2ffa4b0601f814828fb8c07c1270dc11d56d1bb7
AnoopNR4136 <anoopnr94@gmail.com>
2025-12-26 03:58:14 +0530

    fix:Update amount only if from or to month is changed

--- craft_hr/public/js/additional_salary.js
+++ craft_hr/public/js/additional_salary.js
@@ -1,9 +1,9 @@
 frappe.ui.form.on("Additional Salary", {
-    salary_component: function(frm) {
+    salary_component: function (frm) {
         update_reimbursement_amount(frm);
     },
 
-    payroll_date: function(frm) {
+    payroll_date: function (frm) {
         update_reimbursement_amount(frm);
     },
     refresh(frm) {
@@ -13,57 +13,57 @@ frappe.ui.form.on("Additional Salary", {
             });
         }
     },
-    is_recurring: function(frm) {
-		if (frm.doc.is_recurring && frm.doc.ref_doctype === "Employee Advance" && frm.doc.ref_docname) {
-			frappe.call({
-				method: "frappe.client.get",
-				args: {
-					doctype: "Employee Advance",
-					name: frm.doc.ref_docname
-				},
-				callback: function(r) {
-					if (r.message) {
-						let paid = r.message.paid_amount || 0;
-						let claimed = r.message.claimed_amount || 0;
+    is_recurring: function (frm) {
+        if (frm.doc.is_recurring && frm.doc.ref_doctype === "Employee Advance" && frm.doc.ref_docname) {
+            frappe.call({
+                method: "frappe.client.get",
+                args: {
+                    doctype: "Employee Advance",
+                    name: frm.doc.ref_docname
+                },
+                callback: function (r) {
+                    if (r.message) {
+                        let paid = r.message.paid_amount || 0;
+                        let claimed = r.message.claimed_amount || 0;
 
-						let actual = paid - claimed;
+                        let actual = paid - claimed;
 
-						frm.set_value("actual_amount", actual);
-						frm.refresh_field("actual_amount");
-					}
-				}
-			});
-		}
-	},
+                        frm.set_value("actual_amount", actual);
+                        frm.refresh_field("actual_amount");
+                    }
+                }
+            });
+        }
+    },
     from_date(frm) {
-		frm.trigger("calculate_monthly_amount");
-	},
+        frm.trigger("calculate_monthly_amount");
+    },
 
-	to_date(frm) {
-		frm.trigger("calculate_monthly_amount");
-	},
+    to_date(frm) {
+        frm.trigger("calculate_monthly_amount");
+    },
     calculate_monthly_amount(frm) {
-		if (
-			frm.doc.is_recurring &&
-			frm.doc.from_date && 
-			frm.doc.to_date && 
-			frm.doc.actual_amount
-		) {
-			let months = count_unique_months(frm.doc.from_date, frm.doc.to_date);
-			
-			if (months > 0) {
-				let monthly_amount = frm.doc.actual_amount / months;
-				frm.set_value("amount", monthly_amount);
-			}
-		}
-	}
+        if (
+            frm.doc.is_recurring &&
+            frm.doc.from_date &&
+            frm.doc.to_date &&
+            frm.doc.actual_amount
+        ) {
+            let months = count_unique_months(frm.doc.from_date, frm.doc.to_date);
+
+            if (months > 0) {
+                let monthly_amount = frm.doc.actual_amount / months;
+                frm.set_value("amount", monthly_amount);
+            }
+        }
+    }
 });
 
 function update_reimbursement_amount(frm) {
     if (!frm.doc.salary_component || !frm.doc.employee || !frm.doc.payroll_date) return;
 
     frappe.db.get_value("Salary Component", frm.doc.salary_component, [
-        "type", 
+        "type",
         "custom_is_deferred_reimbursement_component"
     ]).then(r => {
         const data = r.message;
@@ -75,7 +75,7 @@ function update_reimbursement_amount(frm) {
                     employee: frm.doc.employee,
                     payroll_date: frm.doc.payroll_date
                 },
-                callback: function(res) {
+                callback: function (res) {
                     if (res.message) {
                         frm.set_value("amount", res.message.amount);
                         frm.refresh_field("amount");
@@ -90,7 +90,7 @@ function count_unique_months(from_date, to_date) {
     let end = new Date(to_date);
 
     let count = 0;
-    
+
     while (start <= end) {
         count++;
         start.setMonth(start.getMonth() + 1);
@@ -120,24 +120,47 @@ function show_update_schedule_dialog(frm) {
         ],
         primary_action_label: "Update",
         primary_action(values) {
-            
-            frappe.call({
-                method: "craft_hr.events.additional_salary.update_recovery_dates",
-                args: {
-                    docname: frm.doc.name,
-                    new_from: values.new_from_date,
-                    new_to: values.new_to_date
-                },
-                callback: function(r) {
-                    if (!r.exc) {
-                        frappe.msgprint("Schedule updated.");
-                        frm.reload_doc();
-                        d.hide();
+            let current_from_date = frm.doc.from_date;
+            let new_from_date = values.new_from_date;
+            let current_to_date = frm.doc.to_date;
+            let new_to_date = values.new_to_date;
+
+            let from_month_changed =
+                getYearMonth(current_from_date) !== getYearMonth(new_from_date);
+
+            let to_month_changed =
+                getYearMonth(current_to_date) !== getYearMonth(new_to_date);
+
+            if (from_month_changed || to_month_changed) {
+                frappe.call({
+                    method: "craft_hr.events.additional_salary.update_recovery_dates",
+                    args: {
+                        docname: frm.doc.name,
+                        new_from: values.new_from_date,
+                        new_to: values.new_to_date
+                    },
+                    callback: function (r) {
+                        if (!r.exc) {
+                            frappe.msgprint("Schedule updated.");
+                            frm.reload_doc();
+                            d.hide();
+                        }
                     }
-                }
-            });
+                });
+            } else {
+                frappe.throw(
+                    __('No month change detected. Please change From Month or To Month to update the schedule.')
+                );
+            }
+
         }
     });
 
     d.show();
 }
+
+// Convert to YYYY-MM for safe comparison
+const getYearMonth = (date) => {
+    return frappe.datetime.str_to_obj(date).getFullYear() + '-' +
+        frappe.datetime.str_to_obj(date).getMonth();
+};
